4c824e69d48f6d792b51230ba836d2b47c2ddf52
MaximeRobeyns <maximerobeyns@gmail.com>
2016-08-27 12:35:03 +0100

    mobile layout set

--- scripts.js
+++ scripts.js
@@ -1,25 +1,47 @@
 var navItems= ["item1", "item2", "item3", "item4"];
 var underlines = ["underline1", "underline2", "underline3", "underline4"];
 var browserCheck = document.getElementById('browserCheck');
+var windowWidth = window.innerWidth;
+var hamburger = document.getElementById('hamburger');
+var first = document.getElementById('first');
+var second = document.getElementById('second');
+var menu = document.getElementById('menu');
+var Xval = 0;
+var Yval = 0;
 
-var browser = navigator.sayswho= (function(){
-    var ua= navigator.userAgent, tem,
-    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if(/trident/i.test(M[1])){
-        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-        return 'IE '+(tem[1] || '');
+function getCoords(e){
+  Xval = e.clientX;
+  Yval = e.clientY
+}
+
+if (windowWidth < 480){
+  navItems.forEach(function(item, index){
+    //console.log("This item is: "+item+" and the key is: "+index);
+    var item = document.getElementById(item);
+    //var itemUnderline = document.getElementById(underlines[index]);
+    item.onmousedown = function(){
+      item.style.backgroundColor = "#F5F5F5";
     }
-    if(M[1]=== 'Chrome'){
-        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
-        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    item.onmouseup = function(){
+      item.style.backgroundColor = "#FFFFFF";
     }
-    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-    return M.join(' ');
-})();
+  });
+}
 
-browserCheck.innerHTML = "Browser check: "+browser;
-
+var hamCount = 0;
+hamburger.onclick = function(){
+  if (hamCount == 0){
+    first.style.opacity = "0";
+    second.style.opacity = "1";
+    menu.style.height = "290px";
+    hamCount=1;
+  } else if (hamCount == 1){
+    first.style.opacity = "1";
+    second.style.opacity = "0";
+    menu.style.height = "0px";
+    hamCount=0;
+  }
+}
 
 navItems.forEach(function(item, index){
   //console.log("This item is: "+item+" and the key is: "+index);
